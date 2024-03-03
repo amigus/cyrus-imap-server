f032ab0f067f38f937136e8dcead5b447b77adde
Adam C. Migus <adam@migus.org>
2024-03-03 11:43:42 -0500

    Be quiet and don't listen! 🔒

--- ldap.sh
+++ ldap.sh
@@ -13,6 +13,6 @@ buildah run $c install -d -m 0755 -g ldap -o ldap /run/slapd
 buildah run $c groupadd -g 478 tls
 buildah run $c usermod -a -G tls ldap
 
-buildah config --cmd '["/usr/sbin/slapd", "-d", "8", "-h", "ldap:/// ldapi://%2Frun%2Fslapd%2Fsocket%2Fldapi ldaps:///", "-g", "ldap", "-u", "ldap"]' $c
+buildah config --cmd '["/usr/sbin/slapd", "-d", "0", "-h", "ldapi://%2Frun%2Fslapd%2Fsocket%2Fldapi", "-g", "ldap", "-u", "ldap"]' $c
 
 buildah commit $c $(basename -s .sh $0)
