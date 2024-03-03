d57b7fc045e5e8e4e10137821d59b1b65a0a9aaf
Adam C. Migus <adam@migus.org>
2024-03-02 19:54:08 -0500

    Externalize relay and relay_recipients

--- postfix.sh
+++ postfix.sh
@@ -2,15 +2,15 @@
 
 c=${1:-$(buildah from base)}
 
-buildah run $c sh -c '\
-zypper in -ly \
+buildah run $c zypper in -ly \
 cyrus-sasl-ldap-auxprop \
 cyrus-sasl-plain \
 cyrus-sasl-scram \
-postfix postfix-ldap && zypper clean -a'
+postfix \
+postfix-ldap
+buildah run $c zypper clean -a
 buildah run $c groupadd -g 478 tls
 buildah run $c usermod -a -G tls postfix
-buildah run $c postmap /etc/postfix/relay /etc/postfix/relay_recipients
 
 buildah config --cmd '["/usr/sbin/postfix", "start-fg"]' $c
 
