ddee2dad6929210a413513e898b89508f02bf02c
Adam C. Migus <adam@migus.org>
2024-03-02 19:53:39 -0500

    Improvements ⚒️ - simplify run expressions - add GSSAPI and sqlite3 - install masssievec, and translatesieve too

--- cyrus-build.sh
+++ cyrus-build.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
-c=${1:-$(buildah from base)}
+c=${1:-$(buildah from base-build)}
 
-buildah run $c sh -c 'zypper in -ly -t pattern devel_C_C++'
-buildah run $c sh -c '\
-zypper in -ly \
+buildah run $c zypper in -ly \
 cyrus-sasl-devel \
-gzip \
-libical-devel \
+libchardet-devel \
 libicu-devel \
 libjansson-devel \
 libopenssl-devel \
-openldap2-devel && zypper clean -a'
-curl -o - -L https://github.com/cyrusimap/cyrus-imapd/releases/download/cyrus-imapd-3.2.11/cyrus-imapd-3.2.11.tar.gz |
+libuchardet-devel \
+perl-Authen-SASL-Cyrus \
+openldap2-devel \
+sqlite3-devel
+buildah run $c zypper clean -a
+curl -Lo - https://github.com/cyrusimap/cyrus-imapd/releases/download/cyrus-imapd-3.2.11/cyrus-imapd-3.2.11.tar.gz |
 buildah run $c tar -zxf - -C /usr/src
+buildah run $c install -d -m 755 -o root -g root /target
 buildah run $c sh -c 'cd /usr/src/cyrus-imapd-3.2.11 && \
 ./configure \
 --prefix=/usr \
---bindir=/usr/cyrus/bin \
---libexecdir=/usr/cyrus/libexec \
+--libexecdir=/usr/lib/cyrus-imapd \
 --enable-autocreate \
 --enable-idled \
 --with-ldap \
+--with-sqlite \
 --without-snmp && \
-make && test 0 -eq $? && \
-mkdir -p /target && \
 DESTDIR=/target make install && \
 for dir in perl/annotator perl/imap perl/sieve/managesieve;
 do ( cd $dir ; make DESTDIR=/target install ); done && \
-install tools/mkimap /target/usr/sbin/'
+cd tools && install masssievec mkimap translatesieve /target/usr/bin/'
 
 buildah commit $c $(basename -s .sh $0)
--- cyrus.sh
+++ cyrus.sh
@@ -2,17 +2,20 @@
 
 c=${1:-$(buildah from base)}
 
-buildah run $c sh -c '\
-zypper in -ly \
+buildah run $c zypper in -ly \
 cyrus-sasl-ldap-auxprop \
+cyrus-sasl-gssapi \
 cyrus-sasl-plain \
 cyrus-sasl-scram \
 libicu-suse65_1 \
+libchardet1 \
+libuchardet0 \
 libjansson4 \
-openssl tar && zypper clean -a'
-
-echo 'lmtp 2003/tcp # Lightweight Mail Transport Protocol service' |
-buildah run $c sh -c 'cat >> /etc/services'
+openssl \
+sqlite3 \
+tar \
+uchardet
+buildah run $c zypper clean -a
 buildah run $(buildah from cyrus-build) tar -C /target -cf - . |
 buildah run $c tar xpf - -C /
 buildah run $c /sbin/ldconfig
@@ -20,6 +23,6 @@ buildah run $c groupadd -g 12 mail
 buildah run $c groupadd -g 478 tls
 buildah run $c useradd -u 76 -g 12 -G tls -d /var/lib/cyrus -M cyrus
 
-buildah config --cmd '["/usr/cyrus/libexec/master", "-D"]' $c
+buildah config --cmd '["/usr/lib/cyrus-imapd/master", "-D"]' $c
 
 buildah commit $c $(basename -s .sh $0)
