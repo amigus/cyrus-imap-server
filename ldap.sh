b8430ead87f250be1dc1d22188c0694b9257b458
Adam C. Migus <adam@migus.org>
2024-03-02 23:54:49 -0500

    Fixes

--- ldap.sh
+++ ldap.sh
@@ -6,13 +6,13 @@ buildah run $c zypper in -ly \
 cyrus-sasl-ldap-auxprop \
 cyrus-sasl-scram \
 openldap2
-buildah run $c zypper clean -a
+buildah run $c zypper clean -a 
 echo 'auxprop_plugin: slapd' |
 buildah run $c sh -c 'cat >| /etc/sasl2/slapd.conf'
 buildah run $c install -d -m 0755 -g ldap -o ldap /run/slapd
 buildah run $c groupadd -g 478 tls
 buildah run $c usermod -a -G tls ldap
 
-buildah config --cmd '["/usr/sbin/slapd", "-d", "8", "-h", "ldap:/// ldaps:///", "-g", "ldap", "-u", "ldap"]' $c
+buildah config --cmd '["/usr/sbin/slapd", "-d", "8", "-h", "ldap:/// ldapi://%2Frun%2Fslapd%2Fsocket%2Fldapi ldaps:///", "-g", "ldap", "-u", "ldap"]' $c
 
 buildah commit $c $(basename -s .sh $0)
